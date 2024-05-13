99ca09df579748e794b321db9b0d0101ba2c8e08
Виктор <Vparipko007@gmail.com>
2024-05-13 23:35:22 +0700

    In

---
+++ AvanpostWeb/Controllers/FilesSearchController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvanpostWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesSearchController : ControllerBase
+    {
+        private readonly ILogger<FilesSearchController> _logger;
+        private readonly string _examplesPath = "examples";
+
+        public FilesSearchController(ILogger<FilesSearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("search")]
+        public IActionResult Search(string word)
+        {
+            try
+            {
+                string[] fileNames = Directory.GetFiles(_examplesPath);
+
+                var filesWithKeyword = fileNames.Where(fileName =>
+                    System.IO.File.ReadAllText(fileName).Contains(word, StringComparison.OrdinalIgnoreCase));
+
+                var foundFileNames = filesWithKeyword.Select(filePath => Path.GetFileName(filePath));
+
+                return new JsonResult(foundFileNames);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при выполнении поиска.");
+                return StatusCode(500, "Произошла ошибка при выполнении поиска.");
+            }
+        }
+    }
+}
